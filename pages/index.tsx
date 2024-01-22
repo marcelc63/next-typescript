c0a6afcd559ae35bbab0ca01f2947287b7f32817
Marcel <marcelc6363@gmail.com>
2024-01-22 11:31:47 +0700

    remove button from index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Button from '~/components/base/Button'
 
 export default function Home() {
   return (
@@ -16,7 +15,6 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <Button>hi</Button>
 
         <p className={styles.description}>
           Get started by editing{' '}
