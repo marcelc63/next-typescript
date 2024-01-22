9a7fb29c45166865f29f8a332aa8348dccaee926
Marcel <marcelc6363@gmail.com>
2024-01-22 12:24:07 +0700

    Fix: Move style from _app.tsx to _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,39 +18,6 @@ const MyApp: React.FC<AppProps> = (props: AppProps) => {
     <Provider store={store}>
       <Head>
         <title>Next Typescript</title>
-        <meta name="description" content="Next with Typescript" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
-          rel="stylesheet"
-        />
-
-        <meta property="og:url" content="https://" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="" />
-        <meta property="og:description" content="" />
-        <meta property="og:image" content="" />
-
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="" />
-        <meta name="twitter:title" content="" />
-        <meta name="twitter:description" content="" />
-        <meta name="twitter:image" content="" />
-
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-`} />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
       </Head>
       <ProtectRoute {...props} />
     </Provider>
